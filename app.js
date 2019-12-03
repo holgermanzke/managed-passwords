72a1ddb0cf0222915816605132645d3bc7bddc40
holgermanzke <manzkeholger@gmail.com>
2019-12-03 10:57:54 +0100

    Add crypto and readline

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
 const { get, set, unset } = require("./utils/cmds");
-
+const { verifyHash, readMasterPassword } = require("./utils/crypto");
 const userArgs = process.argv.slice(2);
 const [cmd, key, value] = userArgs;
+const readline = require("readline");
 
-async function execute() {
+const readlineInterface = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+readlineInterface._writeToOutput = function() {
+  readlineInterface.output.write("");
+};
+readlineInterface.question("Please enter new master password:", password => {
+  execute(password);
+  readlineInterface.close();
+});
+
+async function execute(password) {
+  const hash = readMasterPassword();
+
+  if (!verifyHash(password, hash)) {
+    throw new Error("Invalid master Password");
+  }
   switch (cmd) {
     case "get":
       {
@@ -24,4 +42,4 @@ async function execute() {
       console.log("command not found");
   }
 }
-execute();
+// execute();
